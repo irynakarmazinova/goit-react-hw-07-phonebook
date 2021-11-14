d37aa7f883391c8aa0a25846a47cd49ef989c2a1
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-11-14 20:01:47 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 // import { PersistGate } from 'redux-persist/integration/react';
 import App from "./App";
 // import { store, persistor } from 'redux/store';
-import { store } from "redux/store";
+import { store } from "./redux/store";
 import "modern-normalize/modern-normalize.css";
 import "./index.scss";
 
